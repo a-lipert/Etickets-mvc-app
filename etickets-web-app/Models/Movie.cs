25738a6d2df4c4656d8a254f4afd9fda6dba989b
Aleksandra Lipert <ola.lipert@gmail.com>
2022-06-02 15:43:46 +0200

    Added models and dbcontext

---
+++ etickets-web-app/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using etickets_web_app.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace etickets_web_app.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
+        public MovieCategorie MovieCategorie { get; set; }
+        
+        
+    }
+}

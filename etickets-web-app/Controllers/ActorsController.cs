d0f8c303b6086663e18da53eb6662c03f86a9d04
Aleksandra Lipert <ola.lipert@gmail.com>
2022-06-03 14:14:06 +0200

    Added controllers

---
+++ etickets-web-app/Controllers/ActorsController.cs
@@ -0,0 +1,20 @@
+﻿using etickets_web_app.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace etickets_web_app.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allActors = await _context.Actors.ToListAsync();
+            return View();
+        }
+    }
+}

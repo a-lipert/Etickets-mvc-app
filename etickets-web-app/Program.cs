75ce7b6520200986b5333d11db97c3cdbbf73338
Aleksandra Lipert <ola.lipert@gmail.com>
2022-06-08 10:58:11 +0200

    added navigation bar

--- etickets-web-app/Program.cs
+++ etickets-web-app/Program.cs
@@ -1,4 +1,5 @@
 using etickets_web_app.Data;
+using etickets_web_app.Data.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+builder.Services.AddScoped<IActorsService, ActorsService>();
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())

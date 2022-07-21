5b8e332868d12d972874a0299e066ae2886e0abb
Aleksandra Lipert <ola.lipert@gmail.com>
2022-07-21 14:32:58 +0200

    Added OrdersService

---
+++ etickets-web-app/Data/Services/IOrdersService.cs
@@ -0,0 +1,10 @@
+﻿using etickets_web_app.Models;
+
+namespace etickets_web_app.Data.Services
+{
+    public interface IOrdersService
+    {
+        Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress);
+        Task <List<Order>> GetOrdersByUserIdAsync (string userId);  
+    }
+}

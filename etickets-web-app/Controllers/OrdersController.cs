305f2872bf944225994cd0603d32aba2ae0100e7
Aleksandra Lipert <ola.lipert@gmail.com>
2022-07-21 11:52:19 +0200

    Added shopping cart and orders controller

---
+++ etickets-web-app/Controllers/OrdersController.cs
@@ -0,0 +1,51 @@
+﻿using etickets_web_app.Data.Cart;
+using etickets_web_app.Data.Services;
+using etickets_web_app.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace etickets_web_app.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IMoviesService _moviesService;
+        private readonly ShoppingCart _shoppingCart;
+        public OrdersController(IMoviesService moviesService, ShoppingCart shoppingCart)
+        {
+            _moviesService = moviesService;
+            _shoppingCart = shoppingCart;
+        }
+        public IActionResult ShoppingCart()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+            var response = new ShoppingCartViewModel()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(response);
+        }
+
+        public async Task <IActionResult> AddToShoppingCart(int id)
+        {
+            var item = await _moviesService.GetByIdAsync(id);
+
+            if (item != null)
+            {
+                _shoppingCart.AddItemToCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+
+        public async Task<IActionResult> RemoveFromShoppingCart(int id)
+        {
+            var item = await _moviesService.GetByIdAsync(id);
+
+            if (item != null)
+            {
+                _shoppingCart.RemoveItemFromCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+    }
+}
--- etickets-web-app/Program.cs
+++ etickets-web-app/Program.cs
@@ -1,17 +1,22 @@
 using etickets_web_app.Data;
+using etickets_web_app.Data.Cart;
 using etickets_web_app.Data.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 builder.Services.AddScoped<IActorsService, ActorsService>();
 builder.Services.AddScoped<IProducersService, ProducersService>();
 builder.Services.AddScoped<ICinemasService, CinemasService>();
 builder.Services.AddScoped<IMoviesService, MoviesService>();
 
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
+builder.Services.AddSession();
+
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 
@@ -27,6 +32,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseSession();
 
 app.UseAuthorization();
 

79ed962d3fe66ab7ceef2eac249a98303a8f12ce
Aleksandra Lipert <ola.lipert@gmail.com>
2022-07-28 15:12:20 +0200

    Added login view

---
+++ etickets-web-app/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace etickets_web_app.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email address is required")]
+        [Display(Name = "Email address")]
+        public string EmailAddress { get; set;}
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set;}
+    }
+}
